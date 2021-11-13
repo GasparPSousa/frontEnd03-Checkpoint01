beec71361456b7fb8253caf1d482e57dfc447e73
GasparPSousa <gasparufrj@gmail.com>
2021-11-13 17:20:08 -0300

    Fazendo e estilizando o Banner

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,10 +1,16 @@
-import './style.scss'
+import './style.scss';
 
 const Banner = () => {
   
   return(
-    <secttion id="banner">Banner</secttion>
+    <section id="banner">
+         <div className="foto"> </div>
+      <div className="banner-text">
+        <h2>Muito Prazer!</h2>
+        <p>Meu nome é Gaspar! Sou Engenheiro Mecânico, mas agora estou me jogando no mundo da tecnologia....e estou louco para participar dessa disrupção que está acontecendo no setor financeiro.</p>
+      </div>
+    </section>
   )
 }
 
-export default Banner
+export default Banner;
