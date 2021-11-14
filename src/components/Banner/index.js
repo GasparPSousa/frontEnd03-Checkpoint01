15d06b3b862d40834a4cf2d242dac59cab61ebcd
GasparPSousa <gasparufrj@gmail.com>
2021-11-14 18:20:28 -0300

    Utilizando o Bootstrap para fazer o Footer e estilizando o Banner

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -4,9 +4,9 @@ const Banner = () => {
   
   return(
     <section id="banner">
-         <div className="foto"> </div>
+      <div className="foto"> </div>
       <div className="banner-text">
-        <h2>Muito Prazer!</h2>
+        <h1>Muito Prazer!</h1>
         <p>Meu nome é Gaspar! Sou Engenheiro Mecânico, mas agora estou me jogando no mundo da tecnologia....e estou louco para participar dessa disrupção que está acontecendo no setor financeiro.</p>
       </div>
     </section>
